6cc9a2b584518285c39df52ab73c8389e163ce95
Bernard Huang <bernytest@yahoo.com>
2015-03-20 18:03:44 -0700

    adding regex check for letters only

--- jumble.py
+++ jumble.py
@@ -1,11 +1,15 @@
-import sys
+import sys, re
+
+global userInput
 
 def printInput():
+	global userInput
 	print 'Enter a string: '
-	return raw_input()
+ 	userInput = raw_input()
+	return userInput
 
 def validateInput(inputStr):
-	if len(inputStr.split()) != 1:
+	if len(inputStr.split()) != 1 or not re.match(r'^[a-zA-Z]+$', inputStr):
 		return False
 	else:
 		return True
@@ -13,7 +17,7 @@ def validateInput(inputStr):
 
 #Driver
 
-while not validateInput(userInput = printInput()):
+while not validateInput(printInput()):
 	print 'Improper input, try again...'
 
-
+print 'Valid Input: ' + userInput

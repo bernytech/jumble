a0616dc24adad6ebade250352b057525c0fc3122
Bernard Huang <bernytest@yahoo.com>
2015-03-20 17:46:08 -0700

    added validation for user input

---
+++ jumble.py
@@ -0,0 +1,19 @@
+import sys
+
+def printInput():
+	print 'Enter a string: '
+	return raw_input()
+
+def validateInput(inputStr):
+	if len(inputStr.split()) != 1:
+		return False
+	else:
+		return True
+
+
+#Driver
+
+while not validateInput(printInput()):
+	print 'Improper input, try again...'
+
+
